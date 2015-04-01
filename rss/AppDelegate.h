f50faec5ae2acec0d331fc97d07d30c62c119c6c
akaimo <akaimo@MacBook-Air.local>
2015-04-01 13:15:36 +0900

    Initial Commit

---
+++ rss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rss
+//
+//  Created by akaimo on 2015/04/01.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rss/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  rss
+//
+//  Created by akaimo on 2015/04/01.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
