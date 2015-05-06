629a0c3f1b6f9892bcbe66a890f23f7de2d1eab1
akaimo <akaimo@MacBook-Air-2.local>
2015-05-06 15:31:28 +0900

    webView barItems finish

--- rss/AKAFeedWebViewController.m
+++ rss/AKAFeedWebViewController.m
@@ -8,8 +8,16 @@
 
 #import "AKAFeedWebViewController.h"
 
-@interface AKAFeedWebViewController ()
+@interface AKAFeedWebViewController () <UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@property (strong, nonatomic) UIBarButtonItem *actionBtn;
+@property (strong, nonatomic) UIBarButtonItem *refreshBtn;
+@property (strong, nonatomic) UIBarButtonItem *stopBtn;
+@property (strong, nonatomic) UIBarButtonItem *forwardBtn;
+@property (strong, nonatomic) UIBarButtonItem *backBtn;
+
+@property (nonatomic) int webViewLoads;
 
 @end
 
@@ -22,8 +30,12 @@
     NSURL *url = [NSURL URLWithString:[_feed valueForKey:@"url"]];
     [_webView loadRequest:[NSURLRequest requestWithURL:url]];
     
-    self.title = [_feed valueForKey:@"title"];
-
+    _actionBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(actionBtnTap:)];
+    _refreshBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshBtnTap:)];
+    _stopBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(stopBtnTap:)];
+    _forwardBtn = [[UIBarButtonItem alloc] initWithTitle:@"＞" style:UIBarButtonItemStylePlain target:self action:@selector(forwardBtnTap:)];
+    _backBtn = [[UIBarButtonItem alloc] initWithTitle:@"＜" style:UIBarButtonItemStylePlain target:self action:@selector(backBtnTap:)];
+    self.navigationItem.rightBarButtonItems = @[_actionBtn, _refreshBtn, _forwardBtn, _backBtn];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -31,19 +43,76 @@
     [self.navigationController setToolbarHidden:YES animated:YES];
 }
 
+- (void)viewWillDisappear:(BOOL)animated {
+    if (self.webView.isLoading) {
+        [self.webView stopLoading];
+    }
+    
+    [super viewWillDisappear:animated];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - UIWebViewDelegate
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    /* 戻る・進むボタンのタップ可否を制御 */
+    self.backBtn.enabled = self.webView.canGoBack;
+    self.forwardBtn.enabled = self.webView.canGoForward;
+    
+    /* navigationbarのitemを入れ替える */
+    NSMutableArray *changedBarButtonItemArray = [NSMutableArray arrayWithArray:[self.navigationItem rightBarButtonItems]];
+    [changedBarButtonItemArray replaceObjectAtIndex:1 withObject:_stopBtn];
+    self.navigationItem.rightBarButtonItems = changedBarButtonItemArray;
+    
+    _webViewLoads++;
 }
-*/
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    _webViewLoads--;
+    
+    if (_webViewLoads <= 0) {
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        
+        NSMutableArray *changedBarButtonItemArray = [NSMutableArray arrayWithArray:[self.navigationItem rightBarButtonItems]];
+        [changedBarButtonItemArray replaceObjectAtIndex:1 withObject:_refreshBtn];
+        self.navigationItem.rightBarButtonItems = changedBarButtonItemArray;
+        
+//        NSLog(@"finish");
+    }
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    _webViewLoads--;
+    NSLog(@"error webView");
+}
+
+
+
+
+- (void)actionBtnTap:(UIButton *)sender {
+    
+}
+
+- (void)refreshBtnTap:(UIButton *)sender {
+    [self.webView reload];
+}
+
+- (void)stopBtnTap:(UIButton *)sender {
+    [self.webView stopLoading];
+}
+
+- (void)forwardBtnTap:(UIButton *)sender {
+    if (self.webView.canGoForward) [self.webView goForward];
+}
+
+- (void)backBtnTap:(UIButton *)sender {
+    if (self.webView.canGoBack) [self.webView goBack];
+}
 
 @end
